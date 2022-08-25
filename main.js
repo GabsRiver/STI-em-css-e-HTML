0fe534690cd7435c9b4a6be158112e5f26740230
GabsRiver <gtgabrielxd@gmail.com>
2022-08-24 22:10:44 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 1,
+      setWrapperSize: true
+    }
+  }
+})
